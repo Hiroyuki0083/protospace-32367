3ad0afa871a66473bdd32656c3b2a29f27cc725c
hiroyuki0083 <karasawa5000@icloud.com>
2021-03-03 19:17:35 +0900

    first commit2

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -51,4 +51,8 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
 end
