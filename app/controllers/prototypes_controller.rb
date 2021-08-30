53f7c997531e12376558bc3ef6efd61181b9c0dc
hiroyuki0083 <karasawa5000@icloud.com>
2021-08-30 23:37:17 +0900

    可読性

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
+  
   def index
     @prototypes = Prototype.includes(:user)
   end
